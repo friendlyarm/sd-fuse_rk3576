65564d2c77b213e67d5795a208d5fca5da6de251
Lawrence-Tang <tangzongsheng@gmail.com>
2025-04-08 16:11:51 +0800

    using miniloader which generated by uboot

--- tools/update_uboot_bin.sh
+++ tools/update_uboot_bin.sh
@@ -11,17 +11,13 @@ if [ $# -ne 2 ]; then
 	exit 1
 fi
 
-# FIXME: Do not use the miniloader version of uboot, please use the one inside prebuilt.
-# ---------
-cp -f prebuilt/MiniLoaderAll.bin $2/MiniLoaderAll.bin
-
-# LOADER_DOT_BIN=`ls $1/rk3576_spl_loader_*.bin 2>/dev/null | sort -n | tail -1`
-# if [ -f ${LOADER_DOT_BIN} ]; then
-#     cp -f ${LOADER_DOT_BIN} $2/MiniLoaderAll.bin
-# else
-#     echo "not found $1/rk3576_spl_loader_*.bin, pls build u-boot first."
-#     exit 1
-# fi
+LOADER_DOT_BIN=`ls $1/rk3576_spl_loader_*.bin 2>/dev/null | sort -n | tail -1`
+if [ -f ${LOADER_DOT_BIN} ]; then
+    cp -f ${LOADER_DOT_BIN} $2/MiniLoaderAll.bin
+else
+    echo "not found $1/rk3576_spl_loader_*.bin, pls build u-boot first."
+    exit 1
+fi
 cp -f $1/uboot.img $2/
 
 exit $?
